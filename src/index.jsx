ead4a83b08454f3c23920527a5a746a2fb309e0e
KotoAkira <ruslan.kupryianay@innowise-group.com>
2022-06-01 15:31:52 +0300

    change pre commit hook

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,6 @@ import react from 'react'
 import ReactDom from 'react-dom'
 import App from './app.component'
 
-const rootNode = document.getElementById('root')
+const rootNode =  document.getElementById('root')
 
 ReactDom.render(<App />, rootNode)
